66fa9536de3480a77228dcc412a99bc5044fe986
Mathilde D <m.degris@tudiant.ecolemeilies.fr>
2021-05-07 16:31:20 +0200

    Update Code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float jumpForce;
+
+
     // Start is called before the first frame update
     void Start()
     {
